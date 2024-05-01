347ffd783b434f96036fbe5f4aaee7c723c8d0af
Giacomo <giacomogherardi01@gmail.com>
2024-05-01 16:30:24 +0200

    es2 final

--- Es2/RWexclusive.java
+++ Es2/RWexclusive.java
@@ -1,11 +1,6 @@
-class RWexclusive extends RWbasic {
+public class RWexclusive extends RWbasic {
     @Override
-    public synchronized int read() {
-        return super.read();
-    }
-
-    @Override
-    public synchronized void write() {
-        super.write();
+    public synchronized int write() {
+        return super.write();
     }
 }
